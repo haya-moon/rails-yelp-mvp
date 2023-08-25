8499c971fcd7ea07a838995f9e36524e166b4970
Haya B <lacourt.christelle@gmail.com>
2023-08-25 08:35:04 +0100

    need to finish app

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,18 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  def show
+    @
+  end
+
+end
